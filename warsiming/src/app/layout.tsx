58901dee18d35dc8f820f7a026ad37edca29044e
jp florez <jpfloau@gmail.com>
2025-01-18 20:15:51 -0500

    fixing layout and removing un-needed fluff

--- warsiming/src/app/layout.tsx
+++ warsiming/src/app/layout.tsx
@@ -14,13 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>
-          <h1>War Gaming App</h1>
-        </header>
         <main>{children}</main>
-        <footer>
-          <p>© 2025 War Gaming App</p>
-        </footer>
       </body>
     </html>
   );
